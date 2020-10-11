c8ccffe3fea6ff2374fd12f1f7baea210b9838ff
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-10 22:03:48 -0500

    init setup for Spring, Gridle exercise

---
+++ src/test/java/hello/hellospringgradle/HelloSpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospringgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
