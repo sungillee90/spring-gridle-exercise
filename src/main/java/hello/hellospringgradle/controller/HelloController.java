d518c1515999a87b98cadfd82b2884dfb8c138a0
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-19 09:33:50 -0500

    init setup for first api

--- src/main/java/hello/hellospringgradle/controller/HelloController.java
+++ src/main/java/hello/hellospringgradle/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,4 +23,11 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "Hello " + name;
+    }
+
 }
