09317a5b0eae9415ffcd25b7e48fec4b045f8c8e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-21 19:55:03 -0500

    passed first member save test

--- src/test/java/hello/hellospringgradle/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospringgradle/repository/MemoryMemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package hello.hellospringgradle.repository;
 
+import hello.hellospringgradle.domain.Member;
 import org.junit.jupiter.api.Test;
 
 public class MemoryMemberRepositoryTest {
@@ -7,6 +8,13 @@ public class MemoryMemberRepositoryTest {
 
     @Test
     public void save(){
+        Member member = new Member();
+        member.setName("spring");
 
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+
+        System.out.println("result = " + (result == member));
     }
 }
