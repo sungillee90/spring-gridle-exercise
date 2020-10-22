d9dee62ce3fe86c0026c2c494a300027147f7b6d
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-21 19:52:18 -0500

    init setup for member repo test save

--- src/test/java/hello/hellospringgradle/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospringgradle/repository/MemoryMemberRepositoryTest.java
@@ -1,4 +1,12 @@
 package hello.hellospringgradle.repository;
 
+import org.junit.jupiter.api.Test;
+
 public class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+    }
 }
