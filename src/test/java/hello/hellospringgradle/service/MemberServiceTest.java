d3aece1cc73ff5a0176e11961f267f6589cbca8e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-26 15:09:00 -0500

    test for join

--- src/test/java/hello/hellospringgradle/service/MemberServiceTest.java
+++ src/test/java/hello/hellospringgradle/service/MemberServiceTest.java
@@ -1,13 +1,27 @@
 package hello.hellospringgradle.service;
 
+import hello.hellospringgradle.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
     @Test
     void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(savedId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
