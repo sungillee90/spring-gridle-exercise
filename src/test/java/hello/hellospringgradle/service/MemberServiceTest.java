7a6e7f7bca29c62c9e2751c71cc6b0ebfce7335b
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-27 12:15:59 -0500

    added Before Each to the whole test

--- src/test/java/hello/hellospringgradle/service/MemberServiceTest.java
+++ src/test/java/hello/hellospringgradle/service/MemberServiceTest.java
@@ -4,14 +4,21 @@ import hello.hellospringgradle.domain.Member;
 import hello.hellospringgradle.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService();
+    }
 
     @AfterEach
     public void afterEAch() {
